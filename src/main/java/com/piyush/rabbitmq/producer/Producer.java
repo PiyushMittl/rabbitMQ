efa5fd09edc97c2141ea806e9c5a0670a70bdaf0
PiyushMittal <PiyushMittal@192.168.1.5>
2017-03-18 17:51:01 +0530

    initai commit

---
+++ src/main/java/com/piyush/rabbitmq/producer/Producer.java
@@ -0,0 +1,25 @@
+package com.piyush.rabbitmq.producer;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import org.apache.commons.lang.SerializationUtils;
+
+import com.piyush.rabbitmq.endpoint.EndPoint;
+
+/**
+ * The producer endpoint that writes to the queue.
+ * 
+ * @author syntx
+ *
+ */
+public class Producer extends EndPoint {
+
+	public Producer(String endPointName) throws IOException {
+		super(endPointName);
+	}
+
+	public void sendMessage(Serializable object) throws IOException {
+		channel.basicPublish("", endPointName, null, SerializationUtils.serialize(object));
+	}
+}
